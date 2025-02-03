730827384662d8013da5c0deb17bb6a8b65c24be
k0sseK <k0ssekcs@gmail.com>
2025-02-03 19:51:03 +0100

    remove: delete Notification module and service

--- src/notification/notification.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { NotificationService } from './notiifcation.service'
-
-@Module({
-    providers: [NotificationService],
-    exports: [NotificationService],
-})
-export class NotificationModule {}
